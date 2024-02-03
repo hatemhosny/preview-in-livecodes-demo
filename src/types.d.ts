895c4af4ca9ffc5a353f80057a8396c1e7b622f5
Hatem Hosny <hatemhosny@gmail.com>
2024-02-03 02:14:44 +0200

    fix TS

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,3 @@
 declare module "click-to-celebrate" {
-  export const createCelebrationButton: (container: HTMLElement) => void;
+  export const createCelebrationButton: (container: Element) => void;
 }
